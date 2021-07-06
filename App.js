e3866772702551365f35f246bcac29c646c8b704
galHmi <75162434+galHmi@users.noreply.github.com>
2021-07-06 11:30:27 +0300

    add new text

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>Hello!</Text>
+      <Text>Hello Yarin!</Text>
       <StatusBar style="auto" />
     </View>
   );
